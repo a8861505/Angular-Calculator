1a1bc57458d6058125028121d86fbbc85539a94f
SteveGuo <steveguo0828@gmail.com>
2017-02-15 23:24:27 -0600

    First Edition

---
+++ Michigan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Michigan.Startup))]
+namespace Michigan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
